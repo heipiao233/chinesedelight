53b3e9dda65b6a15fc5afa6e94ba7cd4d3f06f8e
heipiao233 <heipiao233@outlook.com>
2022-12-22 11:15:00 +0800

    fix crash

--- src/main/java/net/heipiao/chinesedelight/item/ModItems.java
+++ src/main/java/net/heipiao/chinesedelight/item/ModItems.java
@@ -15,13 +15,15 @@ public enum ModItems {
     GOLD_SPATULA(() -> new SpatulaItem(ToolMaterials.GOLD, ModItemSettings.noStack().group(ChineseDelight.GROUP))),
     DIAMOND_SPATULA(() -> new SpatulaItem(ToolMaterials.DIAMOND, ModItemSettings.noStack().group(ChineseDelight.GROUP))),
     NETHERITE_SPATULA(() -> new SpatulaItem(ToolMaterials.NETHERITE, ModItemSettings.noStack().group(ChineseDelight.GROUP)));
-    public final Supplier<Item> item;
+    private final Supplier<Item> itemSupplier;
+    public Item item;
     ModItems(Supplier<Item> item) {
-        this.item = item;
+        this.itemSupplier = item;
     }
     public static void register() {
         for (var value: values()) {
-            Registry.register(Registry.ITEM, new Identifier(ChineseDelight.MODID, value.name().toLowerCase()), value.item.get());
+            value.item = value.itemSupplier.get();
+            Registry.register(Registry.ITEM, new Identifier(ChineseDelight.MODID, value.name().toLowerCase()), value.item);
         }
     }
 }
